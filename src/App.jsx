4a13ddcada365d805c29180bf6dd285342f49f8f
vishal-rajpoot <vishalrajpoot540@gmail.com>
2023-12-19 19:21:41 +0530

    first commit here

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import './App.css';
+
+const App = () => {
+    const [count, setCount] = useState(0);
+
+    const increase = () => {
+        setCount(count+1);
+    }
+
+    const decrease = () => {
+        if(count >= 1){
+            setCount(count-1);
+        }
+    }
+
+  return (
+   <>
+    <h1 className= 'head'> count here</h1>
+    <div className='parent'>
+        <button className='inc' onClick = {increase}>+</button>
+        <div className='count'>{count}</div>
+        <button className='inc' onClick={decrease}>-</button>
+    </div>
+    <div>
+        ipsum lorem
+    </div>
+   </>
+  )
+}
+
+export default App
